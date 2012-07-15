c0ab780299634eb9710404f53addb6c7fc23947d
Michael Lapidakis <michael@mlapida.com>
2012-07-15 10:05:41 -0500

    Adjusted model. Changed the name of the Sites model to App. Adjusted the Admin interface. More tweaks to come

---
+++ Sites/admin.py
@@ -0,0 +1,21 @@
+from Sites.models import App
+from Sites.models import Points
+from django.contrib import admin
+
+class PointsInline(admin.StackedInline):
+    model = Points
+    extra = 3
+
+class AppAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['Name']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [PointsInline]
+    list_display = ('Name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name']
+    date_hierarchy = 'pub_date'
+
+admin.site.register(App, AppAdmin)
+
--- Sites/models.py
+++ Sites/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import datetime
 from django.utils import timezone
 
-class Sites(models.Model):
+class App(models.Model):
     Name = models.CharField(max_length=200)
     Address = models.CharField(max_length=200)
     founded = models.IntegerField()
@@ -13,7 +13,7 @@ class Sites(models.Model):
         return self.question
         
 class Points(models.Model):
-    site = models.ForeignKey(Sites)
+    site = models.ForeignKey(App)
     point_type = models.CharField(max_length=50)
     importance = models.IntegerField()
     pub_date = models.DateTimeField('date published')
