80bd67c532e75997483c2e2978ef690584fe4348
Michael Lapidakis <michael@mlapida.com>
2012-07-15 09:43:07 -0500

    Added Model View

--- Sites/models.py
+++ Sites/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import datetime
+from django.utils import timezone
+
+class Sites(models.Model):
+    Name = models.CharField(max_length=200)
+    Address = models.CharField(max_length=200)
+    founded = models.IntegerField()
+    founder = models.CharField(max_length=200)
+    business_model = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __unicode__(self):
+        return self.question
+        
+class Points(models.Model):
+    site = models.ForeignKey(Sites)
+    point_type = models.CharField(max_length=50)
+    importance = models.IntegerField()
+    pub_date = models.DateTimeField('date published')
+    description = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.choice
+
 
 # Create your models here.
--- TeesAndSees/settings.py
+++ TeesAndSees/settings.py
@@ -70,7 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "./Sites/Templates"
+    #"./Sites/Templates"
 )
 
 # List of finder classes that know how to find static files in
@@ -156,4 +156,4 @@ LOGGING = {
 }
 
 import dj_database_url
-DATABASES = {'default': dj_database_url.config(default='postgres://postgres:coolooc23@localhost:5432/TeesAndSees')}
+DATABASES = {'default': dj_database_url.config(default='postgres://postgres:coolooc23@localhost:5432/teesandsees')}
