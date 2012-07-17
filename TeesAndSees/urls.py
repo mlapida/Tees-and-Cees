600839f9488e275f032d057964812fe8d1e8eed0
Michael Lapidakis <michael@mlapida.com>
2012-07-16 22:36:49 -0500

    Replaced the home view with the search view. Made the default url search. Adjusted search results page.

--- TeesAndSees/urls.py
+++ TeesAndSees/urls.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^$', 'Sites.views.index'),
+	#url(r'^$', 'Sites.views.index'),
+	url(r'^$', include('haystack.urls')),
     url(r'^apps/', include('Sites.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    (r'^search/', include('haystack.urls')),
+    url(r'^search/', include('haystack.urls')),
 )
