2dcff450ff1c78c6e5dd2512cc31ff35aec2bdb9
Michael Lapidakis <michael@mlapida.com>
2012-07-17 21:26:36 -0500

    trying out gunicorn some more

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn TeesAndSees.wsgi -b 0.0.0.0:$PORT
--- TeesAndSees/settings.py
+++ TeesAndSees/settings.py
@@ -70,7 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "./Sites/Static",
+    "/Sites/Static",
 )
 
 # List of finder classes that know how to find static files in
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'Sites',
     'haystack',
+    'gunicorn',
 )
 
 # A sample logging configuration. The only tangible logging
