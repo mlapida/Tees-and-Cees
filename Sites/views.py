4eed584df60c74c5bda9f658423601ef2c7215d8
Michael Lapidakis <michael@mlapida.com>
2012-07-18 20:36:22 -0500

    Made home page dynamic and seperated search page

--- Sites/views.py
+++ Sites/views.py
@@ -2,6 +2,9 @@
 from django.template import Context, loader
 from Sites.models import Page
 from django.http import HttpResponse
+from haystack.query import SearchQuerySet
+from haystack.views import search_view_factory
+from haystack.views import SearchView
 
 def index(request):
     homepage_list = Page.objects.filter(Dest='home').order_by('-pub_date')[:5]
@@ -10,3 +13,19 @@ def index(request):
         'homepage_list': homepage_list,
     })
     return HttpResponse(t.render(c))
+    
+def search(request):
+    sqs = SearchQuerySet().all()
+    homepage_list = Page.objects.filter(Dest='home').order_by('-pub_date')[:5]
+    
+    extra_context = Context({
+        'homepage_list': homepage_list,
+    })
+    
+    view = search_view_factory(
+        view_class=SearchView,
+        template='search/search.html',
+        searchqueryset=sqs,
+        )
+    return view(request)	
+    
--- TeesAndSees/urls.py
+++ TeesAndSees/urls.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	#url(r'^$', 'Sites.views.index'),
-	url(r'^$', include('haystack.urls')),
+	url(r'^$', 'Sites.views.index'),
+	#url(r'^$', include('haystack.urls')),
     url(r'^apps/', include('Sites.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^search/', include('haystack.urls')),
+    url(r'^search/', 'Sites.views.search'),
 )
 
